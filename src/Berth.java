1ffece910ad7a11b723ac8c6ea6faafe1ae61a64
Luke Ceddia <flukiluke@gmail.com>
2019-03-25 22:21:45 +1100

    Simplify locking code for shield

--- src/Berth.java
+++ src/Berth.java
@@ -7,38 +7,28 @@
  * @author Luke Ceddia [834076]
  */
 public class Berth extends Location {
-  // State of the shield; docking & undocking must wait for deactivated shield.
-  private volatile boolean shieldActivated = false;
-
   /**
    * Activate the shield, and prevent and docking or undocking manoeuvres from
    * starting. The shield lasts for Params.DEBRIS_TIME. This function only
    * returns once the shield has expired.
    */
   public synchronized void triggerShield() throws InterruptedException {
-    shieldActivated = true;
     System.out.println("Shield is activated.");
-    notifyAll();
+    // Thread.sleep does not release intrinsic locks, so arrive & depart can't
+    // execute while this method is active. This is the mechanism to prevent
+    // docking & undocking while the shield is active.
     Thread.sleep(Params.DEBRIS_TIME);
-    shieldActivated = false;
     System.out.println("Shield is deactivated.");
-    notifyAll();
   }
 
   @Override
   public synchronized void arrive(Ship ship) throws InterruptedException {
-    while (shieldActivated) {
-      wait();
-    }
     super.arrive(ship);
     System.out.format("ship [%d] docks at berth.\n", ship.id);
   }
 
   @Override
   public synchronized Ship depart() throws InterruptedException {
-    while (shieldActivated) {
-      wait();
-    }
     Ship ship = super.depart();
     System.out.format("ship [%d] undocks from berth.\n", ship.id);
     return ship;
